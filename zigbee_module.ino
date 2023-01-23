adcf667cc69f4efeb0b37a63afe666bc19edbf2d
ABHISHEK BANSODE <88792504+Abhishek-Bansode@users.noreply.github.com>
2023-01-23 14:33:07 +0530

    Add files via upload

---
+++ zigbee_module.ino
@@ -0,0 +1,29 @@
+#include <SoftwareSerial.h>
+SoftwareSerial xbeeSerial(2,3); //RX, TX
+
+void setup() {
+   Serial.begin(9600);
+   xbeeSerial.begin(9600);
+}
+
+void loop() {
+   if(Serial.available() > 0){
+      char input = xbeeSerial.read();
+      xbeeSerial.print(input);
+   }
+}
+
+//#include <SoftwareSerial.h>
+//SoftwareSerial xbeeSerial(2,3); //RX, TX
+//
+//void setup() {
+//   Serial.begin(9600);
+//   xbeeSerial.begin(9600);
+//}
+//
+//void loop() {
+//   if(xbeeSerial.available() > 0){
+//      char input = xbeeSerial.read();
+//      Serial.print(input);
+//   }
+//}
