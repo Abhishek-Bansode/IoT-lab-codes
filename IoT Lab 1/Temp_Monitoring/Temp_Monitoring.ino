40fd2c3e357542548f728821f2e89b992c774db4
ABHISHEK BANSODE <88792504+Abhishek-Bansode@users.noreply.github.com>
2023-01-23 13:15:11 +0530

    Add files via upload

---
+++ IoT Lab 1/Temp_Monitoring/Temp_Monitoring.ino
@@ -0,0 +1,21 @@
+// study the temprature sensor and WAP to monitor temprature using Arduino
+
+
+float temp;
+int tempPin = 0;
+
+void setup() {
+   Serial.begin(9600);
+}
+
+void loop() {
+   temp = analogRead(tempPin);
+   // read analog volt from sensor and save to variable temp
+   temp = temp * 0.48828125;
+   // convert the analog volt to its temperature equivalent
+   Serial.print("TEMPERATURE = ");
+   Serial.print(temp); // display temperature value
+   Serial.print("*C");
+   Serial.println();
+   delay(1000); // update sensor reading each one second
+}
